e31013f806df81f9df2824896294c927962d6d2c
MAYANK BHATT <61897123+mayankbhatt07141@users.noreply.github.com>
2020-11-26 01:02:20 +0530

    Create is_prime.cpp
    program for prime number which have O(sqrt(n)) time complexity

---
+++ is_prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool is_prime(int n){
+	for(int i=2;i<=sqrt(n);i++){
+		if(n%i==0)
+			return false;
+		else
+			return true;	
+	}
+}
+int main(){
+	int n;
+	cout <<"enter the number";
+	cin >> n;
+	cout << is_prime(n); //prints 1 if true (prime) and 0 if false(not prime)
+	
+}
