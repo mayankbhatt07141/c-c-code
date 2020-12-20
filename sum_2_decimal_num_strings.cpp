990bfecfe70ef311fcb576455a773c6e75cfa06e
MAYANK BHATT <61897123+mayankbhatt07141@users.noreply.github.com>
2020-12-20 14:09:02 +0530

    Create sum_2_decimal_num_strings.cpp
    if the numbers are too large enough that an long long int can not hold then we create the string of both numbers and stores the sum also in a string .

---
+++ sum_2_decimal_num_strings.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void sum(string a, string b){
+	reverse(a.begin(),a.end());
+	reverse(b.begin(),b.end());
+	string c="";
+	int carry=0,sum=0;
+	int i=0;
+	int j=0;
+	while(i<a.size()||j<b.size()){
+		int x1=(a[i]-'0');
+		int x2=(b[i]-'0');
+		sum =x1+x2+carry;
+		carry=sum/10;
+		sum=sum%10;
+		char ch=(sum+'0');
+		c=c+ch;
+		i++;
+		j++;
+	}
+	if(carry){
+		char ch=(carry+'0');
+		c=c+ch;
+	}
+	reverse(c.begin(),c.end());
+	cout<<c;
+}
+int main() {
+	
+	string a;
+	string b;
+	cin>>a;
+	cin>>b;
+	sum(a,b);
+}
