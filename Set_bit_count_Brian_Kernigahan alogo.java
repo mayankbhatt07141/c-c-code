2bbf64be3018defacf7e860d5c73429431ed8cda
MAYANK BHATT <61897123+mayankbhatt07141@users.noreply.github.com>
2021-01-23 16:04:08 +0530

    Update and rename Set_bit_count_Brain_Kernigahan.java to Set_bit_count_Brian_Kernigahan alogo.java

--- Set_bit_count_Brian_Kernigahan alogo.java
+++ Set_bit_count_Brian_Kernigahan alogo.java
@@ -1,4 +1,4 @@
-/*Subtracting 1 from a decimal number flips all the bits after the rightmost set bit(which is 1) including the rightmost set bit.
+/* According to brain Kernigahan -Subtracting 1 from a decimal number flips all the bits after the rightmost set bit(which is 1) including the rightmost set bit.
 example-
 10 in binary is 00001010  
 9 in binary is 00001001 turned all bits 1 from rightmost set bit (turned 0)*/
