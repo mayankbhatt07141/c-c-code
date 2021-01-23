a7f583c992358cb3405e7042104603db44874d5a
MAYANK BHATT <61897123+mayankbhatt07141@users.noreply.github.com>
2021-01-23 16:01:05 +0530

    Create Set_bit_count_Brain_Kernigahan.java

---
+++ Set_bit_count_Brain_Kernigahan.java
@@ -0,0 +1,17 @@
+/*Subtracting 1 from a decimal number flips all the bits after the rightmost set bit(which is 1) including the rightmost set bit.
+example-
+10 in binary is 00001010  
+9 in binary is 00001001 turned all bits 1 from rightmost set bit (turned 0)*/
+
+public class SetbitCount{
+  public sattic void main(String agrs[]){
+      int n=10;
+      int result=0;
+      while(n>0){
+          n=n&(n-1);
+          result++;
+      }
+      System.out.println("set bits are"+result);
+  }   
+}
+
